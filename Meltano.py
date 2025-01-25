86dd1b914bf7f153919a23831fe2a8e84faa003f
Maysio Saboia <maysio.saboia@hotmail.com>
2025-01-25 00:10:19 -0400

    Update Meltano.py

--- Meltano.py
+++ Meltano.py
@@ -1,1 +1,21 @@
+pip install meltano
+
+meltano init meu_projeto
+cd meu_projeto
+
+meltano add extractor tap-postgres
+meltano add loader target-csv
+
+extractors:
+- name: tap-postgres
+  config:
+    host: "host"
+    port: "porta"
+    database: "nome_do_banco"
+    user: "usuario"
+    password: "senha"
+loaders:
+- name: target-csv
+  config:
+    destination_path: "/path/to/save/csv"
 
