4190d012382bf3ee811a2e0b581dfa2a0000ad30
Maysio Saboia <maysio.saboia@hotmail.com>
2025-01-25 12:21:13 -0400

    Update and rename before-first-use-installs.py to run-before-first-use-as-sudo.py

---
+++ run-before-first-use-as-sudo.py
@@ -0,0 +1,15 @@
+apt install python3.12-venv
+
+#Install docker
+
+#Install Apache Airflow
+python3 -m venv airflow_venv
+source airflow_venv/bin/activate
+pip install --upgrade pip setuptools
+pip install apache-airflow
+
+
+
+pip3 install apache-airflow --break-system-packages
+
+#Install Meltano
