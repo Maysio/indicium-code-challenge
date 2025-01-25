7539218e0b406631934ee192169c86308fa1d861
Maysio Saboia <maysio.saboia@hotmail.com>
2025-01-25 12:28:17 -0400

    Update run-before-first-use-as-sudo.sh

--- run-before-first-use-as-sudo.sh
+++ run-before-first-use-as-sudo.sh
@@ -1,15 +1,8 @@
-sudo apt install python3.12-venv
+sudo apt install python3-pip
 
 #Install docker
 
 #Install Apache Airflow
-python3 -m venv airflow_venv
-source airflow_venv/bin/activate
-pip install --upgrade pip setuptools
-pip install apache-airflow
-
-
-
 pip3 install apache-airflow --break-system-packages
 
 #Install Meltano
