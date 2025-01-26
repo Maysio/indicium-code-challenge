1b16e3038935194e185957684bb64253c8a135c2
Maysio Saboia <maysio.saboia@hotmail.com>
2025-01-26 16:38:08 -0400

    Update run-before-first-use-as-sudo.sh

--- run-before-first-use-as-sudo.sh
+++ run-before-first-use-as-sudo.sh
@@ -3,22 +3,22 @@
 ###Install docker
 sudo apt update
 sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
-docker –version
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose –version
-pwd
+cd ~
 git clone https://github.com/Maysio/indicium-code-challenge
 cd indicium-code-challenge/
-ls
-docker-compose up
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+docker-compose up -d
 
 ###Install Apache Airflow
 #pip3 install apache-airflow --break-system-packages
