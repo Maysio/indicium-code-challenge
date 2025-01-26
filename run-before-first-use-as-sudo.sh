ae26ebe6f0314540305b3c4ffdd1c80dd0ce8347
Maysio Saboia <maysio.saboia@hotmail.com>
2025-01-26 16:56:16 -0400

    Update run-before-first-use-as-sudo.sh

--- run-before-first-use-as-sudo.sh
+++ run-before-first-use-as-sudo.sh
@@ -1,4 +1,4 @@
-#sudo apt install python3-pip -y
+sudo apt install python3-pip -y
 
 ###Install docker
 sudo apt update
@@ -12,14 +12,16 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-#sudo groupadd docker
-#sudo usermod -aG docker $USER
-sudo docker-compose up -d
+sudo groupadd docker
+sudo usermod -aG docker $USER
+#sudo docker-compose up -d
 
 ###Install Apache Airflow
-#pip3 install apache-airflow --break-system-packages
-#pip3 install pandas --break-system-packages
-#pip3 install psycopg2 --break-system-packages
-#pip3 install psycopg2-binary --break-system-packages
+pip3 install apache-airflow --break-system-packages
+pip3 install pandas --break-system-packages
+pip3 install psycopg2 --break-system-packages
+pip3 install psycopg2-binary --break-system-packages
 
 ###Install Meltano
+
+sudo reboot
