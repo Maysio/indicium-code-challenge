f4e2fc8fd9cb071e3bb617923bf486bf61c6c163
Maysio Saboia <maysio.saboia@hotmail.com>
2025-01-26 16:14:25 -0400

    Update run-before-first-use-as-sudo.sh

--- run-before-first-use-as-sudo.sh
+++ run-before-first-use-as-sudo.sh
@@ -1,11 +1,29 @@
-sudo apt install python3-pip -y
+#sudo apt install python3-pip -y
 
 ###Install docker
+sudo apt update
+sudo apt upgrade
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+docker –version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose –version
+pwd
+git clone https://github.com/Maysio/indicium-code-challenge
+cd indicium-code-challenge/
+ls
+docker-compose up
 
 ###Install Apache Airflow
-pip3 install apache-airflow --break-system-packages
-pip3 install pandas --break-system-packages
-pip3 install psycopg2 --break-system-packages
-pip3 install psycopg2-binary --break-system-packages
+#pip3 install apache-airflow --break-system-packages
+#pip3 install pandas --break-system-packages
+#pip3 install psycopg2 --break-system-packages
+#pip3 install psycopg2-binary --break-system-packages
 
 ###Install Meltano
