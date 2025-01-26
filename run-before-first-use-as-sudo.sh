0fb7b42077f31f8d93c42beee6a1857976e10c77
Maysio Saboia <maysio.saboia@hotmail.com>
2025-01-26 16:38:46 -0400

    Update run-before-first-use-as-sudo.sh

--- run-before-first-use-as-sudo.sh
+++ run-before-first-use-as-sudo.sh
@@ -12,9 +12,9 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-cd ~
-git clone https://github.com/Maysio/indicium-code-challenge
-cd indicium-code-challenge/
+#cd ~
+#git clone https://github.com/Maysio/indicium-code-challenge
+#cd indicium-code-challenge/
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
